88324c6d3a0832b2af6c009cb5ae9e5f7dc4b3f0
Robson Peterson <robsonpeterson@hotmail.com>
2021-11-01 21:07:05 -0300

    implementando h2 e jpa

--- src/main/java/br/com/robson/forum/modelo/Resposta.java
+++ src/main/java/br/com/robson/forum/modelo/Resposta.java
@@ -2,12 +2,23 @@ package br.com.robson.forum.modelo;
 
 import java.time.LocalDateTime;
 
-public class Resposta {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 
-	private Long id;
+@Entity
+public class Resposta {
+	
+	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;	
 	private String mensagem;
+	
+	@ManyToOne
 	private Topico topico;
 	private LocalDateTime dataCriacao = LocalDateTime.now();
+	@ManyToOne
 	private Usuario autor;
 	private Boolean solucao = false;
 
